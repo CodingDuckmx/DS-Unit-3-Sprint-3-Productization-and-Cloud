54af833c8adb0363ecc1060176e47463a9599bc4
Coding Duck <57272996+CodingDuckmx@users.noreply.github.com>
2020-03-24 13:18:51 -0400

    basilica and twitter services added

--- module1-web-application-development-with-flask/app-demo/web_app/services/basilica_services.py
+++ module1-web-application-development-with-flask/app-demo/web_app/services/basilica_services.py
@@ -6,9 +6,21 @@ import basilica
 
 load_dotenv()
 
-API_KEY = os.getenv('BASISILICA_API_KEY')
+API_KEY = os.getenv('BASILICA_API_KEY')
 
-with basilica.Connection(API_KEY) as c:
-    embeddings = c.embed_sentences(["Hello world!", "How are you?"])
-    print('----------------')
-    print(list(embeddings)) # [[0.8556405305862427, ...], ...]
+connection = basilica.Connection(API_KEY)
+
+sentence = 'Hey you!'
+sent_embeddings = connection.embed_sentence(sentence)
+print('----------------')
+print(sent_embeddings)
+
+print('----------------')
+print('----------------')
+
+sentences = ["Hello world!", "How are you?"]
+embeddings = connection.embed_sentences(sentences)
+print('----------------')
+print('EMBEDDINGS:')
+print(type(embeddings))
+print(list(embeddings)) # [[0.8556405305862427, ...], ...]
